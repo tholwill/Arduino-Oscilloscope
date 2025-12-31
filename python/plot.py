8f2931c5edd70014b4e10a0e1402c894d71f32c4
Tom Holwill <thomasholwill@outlook.com>
2025-12-30 22:51:43 -0800

    Added random noise for sampling without arduino

--- python/plot.py
+++ python/plot.py
@@ -1,7 +1,10 @@
 import serial
+import numpy as np
+import random #remove when actually implemented
 import matplotlib.pyplot as plt
 from collections import deque
 import time
+import math #remove when actually implemented
 
 port = 'COM3' #REPLACE WITH RELEVANT PORT
 baud_rate = 115200
@@ -19,9 +22,14 @@ last_render = time.perf_counter()
 waves_displayed = 3
 period = None
 
-x = list(range(buffer_size))
+edge_state = 0; # 0 = LOW, 1 = HIGH
+
+x = list(range(0,buffer_size))
 fig, ax = plt.subplots()
 (line,) = ax.plot(x, [0]*buffer_size, color="blue")
+ax.set_xlim(0,buffer_size)
+plt.show(block=False)
+
 
 ax.set_xlim(0,5000) 
 ax.set_ylim(0,10) #10V is the hypothetical max voltage the device can read
@@ -34,50 +42,73 @@ def updateFigure(data):
     
     param data: a list or deque containing all the data to be displayed
     '''
-    y = list(data)
+    y = np.full(5000, np.nan)
+    n = min(len(data), 5000)
+    y[:n] = data[:n]
 
-    if  len(y) < buffer_size:
-        y = [0] * (buffer_size - len(y)) + y
 
     line.set_ydata(y)
     fig.canvas.draw_idle()
-    fig.canvas.flush_events()
+    plt.pause(0.001)
 
-def risingEdgeDetection(data, threshold = 2) -> bool:
+
+def risingEdgeDetection(sample, state, low = 1.8, high = 2.2) -> bool:
     '''
-    detects a threshold crossing rising edge
+    uses a schmitt trigger to detech a rising edge action in the signal
     
-    :param data: a list or deque containing all the data
-    :param threshold: the voltage threshold to see if crossed
-    :return: returns bool depending on if the last point in data is a rising edge
+    :param data: sample being tested for rising edge
+    :param state: the current state of the program (high/low)
+    :return: returns bool that reflects the new state
     :rtype: bool
     '''
-    if len(data) < 2:
-        return False
+    if state == 0 and sample >= high:
+        return True, 1
 
-    return data[-2] < threshold and data[-1] >= threshold #return if data has crossed from below to above the threshold
+    if state == 1 and sample <= low:
+        return False, 0
+
+    return False, state
+
+
+fs = 10_000        # 10 kHz sample rate
+dt = 1 / fs
+t = 0.0
+signal_freq = 2   # 2 Hz
 
 try:
+    '''
     #connect to serial port
     print(f"Attempting to establish connection to {port} at {baud_rate}.")
     ser = serial.Serial(port, baud_rate)
     time.sleep(2) #time for connection to properly establish
     print(f"Connection established successfully.")
-
+    '''
     while True:
         #read new data
+        '''
         raw_data = ser.readline()
         decoded_data = raw_data.decode("utf-8").strip()
-
+        
         try:
             sample = 2.0 * float(decoded_data) * 5.0 / 1023.0
         except ValueError:
             continue
+        '''
+        noise_amplitude = 0.2  # volts (adjust)
 
+        sample = (
+            2.5
+            + 2.5 * math.sin(2 * math.pi * signal_freq * t)
+            + random.uniform(-noise_amplitude, noise_amplitude)
+        )
+
+        t += dt
         raw_buffer.append(sample)
 
+        edge_detected, edge_state = risingEdgeDetection(sample, edge_state)
+
         #detect rising edge
-        if risingEdgeDetection(raw_buffer):
+        if edge_detected:
             if triggered_rising_edge == 0:
                 #first edge: arm capture
                 triggered_rising_edge = 1
@@ -86,7 +117,6 @@ try:
             else: # if not the first edge, must be second edge -> measure period
                 period = period_sample_count
                 period_sample_count = 0
-                triggered_rising_edge += 1
 
         #Capture samples after trigger
         if triggered_rising_edge:
@@ -99,6 +129,7 @@ try:
                 if len(display_buffer) >= max_samples:
                     triggered_rising_edge = 0
                     period_sample_count = 0
+                    period = None
 
         #update figure
         now = time.perf_counter()
